226e6022d6c0119839e23f243e6be09c40a796a1
sandro <sandro@lwhttpd.com>
2023-12-04 23:35:09 +0800

    BPi-R3 Mini: fix error

--- target/linux/rockchip/armv8/base-files/etc/usr/bin/uqmi-daemon.sh
+++ target/linux/rockchip/armv8/base-files/etc/usr/bin/uqmi-daemon.sh
@@ -2,6 +2,7 @@
 
 QMI_DEV=/dev/cdc-wdm0
 QMI_LOG=/var/log/uqmi_daemon.log
+MAX_FAILED_COUNT=10
 
 function set_device_operating_mode() {
 	var=$(uqmi -d $1 --set-device-operating-mode=$2)
@@ -26,20 +27,28 @@ function ping_domain() {
 	fi
 }
 
-echo /dev/null > $QMI_LOG
-echo "$(date '+%Y-%m-%d %H:%M:%S'): QMI daemon is started..." > $QMI_LOG
+echo /dev/null > ${QMI_LOG}
+echo "$(date '+%Y-%m-%d %H:%M:%S'): QMI daemon is started..." > ${QMI_LOG}
 
+FAILED_COUNT=0
 NETWORK_STATUS=false
 
 while true; do
 	if [ $(ping_domain) = "false" ]; then
-		echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is unreachable, try to reset" >> $QMI_LOG
+		((FAILED_COUNT+=1))
+		if [ ${FAILED_COUNT} -ge ${MAX_FAILED_COUNT} ]; then
+			# reboot
+			echo "$(date '+%Y-%m-%d %H:%M:%S'): Network cannot be restored! restart the router..." >> ${QMI_LOG}
+			/sbin/reboot
+		fi
+		echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is unreachable, try to reset" >> ${QMI_LOG}
 		set_device_operating_mode $QMI_DEV reset
 		sleep 30
 	else
-		if [ $NETWORK_STATUS = "false" ]; then
+		if [ ${NETWORK_STATUS} = "false" ]; then
 			NETWORK_STATUS=true
-			echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is reachable" >> $QMI_LOG
+			FAILED_COUNT=0
+			echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is reachable" >> ${QMI_LOG}
 		fi
 	fi
 	sleep 1800
