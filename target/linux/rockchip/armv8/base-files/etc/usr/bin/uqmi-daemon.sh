e324426d4750f4539d68387b6bec73a4b5204b0b
YukPingFong <nereus@lwhttpd.com>
2023-12-10 16:15:05 +0800

    uqmi daemin

--- target/linux/rockchip/armv8/base-files/etc/usr/bin/uqmi-daemon.sh
+++ target/linux/rockchip/armv8/base-files/etc/usr/bin/uqmi-daemon.sh
@@ -5,26 +5,30 @@ QMI_LOG=/var/log/uqmi_daemon.log
 MAX_FAILED_COUNT=10
 
 function set_device_operating_mode() {
-	var=$(uqmi -d $1 --set-device-operating-mode=$2)
+    var=$(uqmi -d $1 --set-device-operating-mode=$2)
 }
 
 function ping_domain() {
-	local domain=223.5.5.5
-	local retries=6
-	local packets_responded=0
+	local dnspod=119.29.29.29
+	local alidns=223.5.5.5
+    local retries=6
+    local packets_responded=0
 
-	for i in $(seq 1 $retries); do
-		if ping -c 1 $domain >/dev/null; then
+    for i in $(seq 1 $retries); do
+        if ping -c 4 ${dnspod} > /dev/null; then
+            ((packets_responded++))
+            sleep 3
+		elif ping -c 4 ${alidns} > /dev/null; then
 			((packets_responded++))
-			sleep 1
-		fi
-	done
+            sleep 3
+        fi
+    done
 
-	if [ $packets_responded -ge 2 ]; then
-		echo "true"
-	else
-		echo "false"
-	fi
+    if [ ${packets_responded} -ge 2 ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
 }
 
 echo /dev/null > ${QMI_LOG}
@@ -34,22 +38,23 @@ FAILED_COUNT=0
 NETWORK_STATUS=false
 
 while true; do
-	if [ $(ping_domain) = "false" ]; then
-		((FAILED_COUNT+=1))
-		if [ ${FAILED_COUNT} -ge ${MAX_FAILED_COUNT} ]; then
-			# reboot
-			echo "$(date '+%Y-%m-%d %H:%M:%S'): Network cannot be restored! restart the router..." >> ${QMI_LOG}
-			/sbin/reboot
-		fi
-		echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is unreachable, try to reset" >> ${QMI_LOG}
-		set_device_operating_mode $QMI_DEV reset
-		sleep 30
-	else
-		if [ ${NETWORK_STATUS} = "false" ]; then
-			NETWORK_STATUS=true
-			FAILED_COUNT=0
-			echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is reachable" >> ${QMI_LOG}
-		fi
-	fi
-	sleep 1800
+    if [ $(ping_domain) = "false" ]; then
+        ((FAILED_COUNT+=1))
+        if [ ${FAILED_COUNT} -ge ${MAX_FAILED_COUNT} ]; then
+            # reboot
+            echo "$(date '+%Y-%m-%d %H:%M:%S'): Network cannot be restored! restart the router..." >> ${QMI_LOG}
+            /sbin/reboot
+        fi
+        echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is unreachable, try to reset" >> ${QMI_LOG}
+        set_device_operating_mode ${QMI_DEV} reset
+        sleep 30
+    else
+        if [ ${NETWORK_STATUS} = "false" ]; then
+            NETWORK_STATUS=true
+            FAILED_COUNT=0
+            echo "$(date '+%Y-%m-%d %H:%M:%S'): Network is reachable" >> ${QMI_LOG}
+        fi
+    fi
+    sleep 1800
 done
+
