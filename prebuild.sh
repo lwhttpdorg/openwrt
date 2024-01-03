423b006c728061800bcfc99d7ee15a604cdfb991
sandro <sandro@lwhttpd.com>
2024-01-04 00:12:31 +0800

    fix error

--- prebuild.sh
+++ prebuild.sh
@@ -9,6 +9,7 @@
 #sed -i -e 's/GO_VERSION_PATCH:=[0-9]\+/GO_VERSION_PATCH:=10/' $GOLANG_MAKEFILE
 #sed -i -e 's/PKG_HASH:=[0-9a-f]\+/PKG_HASH:=72d2f51805c47150066c103754c75fddb2c19d48c9219fa33d1e46696c841dbb/' $GOLANG_MAKEFILE
 
+rm -rf ./feeds/packages/lang/golang/golang/patches
 git apply ./golang-version.diff
 
 ./scripts/feeds install -a
