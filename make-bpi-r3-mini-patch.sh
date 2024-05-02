f9bf0217527619d2272b8e894a4658d9a760c7f5
sandro <sandro@lwhttpd.com>
2024-05-02 19:01:44 +0800

    Fix: RM520N cannot be detected

--- make-bpi-r3-mini-patch.sh
+++ make-bpi-r3-mini-patch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# make diff of mt7986a.dtsi
-diff -u target/linux/mediatek/files-5.15/arch/arm64/boot/dts/mediatek/mt7986a.dtsi ./mt7986a.dtsi > ./mt7986a.patch
-# replace diff head
-sed -i '1,2d' ./mt7986a.patch
-sed -i '1i --- a/arch/arm64/boot/dts/mediatek/mt7986a.dtsi' ./mt7986a.patch
-sed -i '1a +++ b/arch/arm64/boot/dts/mediatek/mt7986a.dtsi' ./mt7986a.patch
+# # make diff of mt7986a.dtsi
+# diff -u target/linux/mediatek/files-5.15/arch/arm64/boot/dts/mediatek/mt7986a.dtsi ./mt7986a.dtsi > ./mt7986a.patch
+# # replace diff head
+# sed -i '1,2d' ./mt7986a.patch
+# sed -i '1i --- a/arch/arm64/boot/dts/mediatek/mt7986a.dtsi' ./mt7986a.patch
+# sed -i '1a +++ b/arch/arm64/boot/dts/mediatek/mt7986a.dtsi' ./mt7986a.patch
 
 
 # make diff of mt7986a-bananapi-bpi-r3-mini.dts
@@ -16,11 +16,11 @@ sed -i '1i --- a/dev/null' ./mt7986a-bananapi-bpi-r3-mini.patch
 sed -i '1a +++ b/arch/arm64/boot/dts/mediatek/mt7986a-bananapi-bpi-r3-mini.dts' ./mt7986a-bananapi-bpi-r3-mini.patch
 
 # merge diff
-cat ./mt7986a.patch > ./196-dts-mt7986a-bpi-r3-mini.patch
-cat ./mt7986a-bananapi-bpi-r3-mini.patch >> ./196-dts-mt7986a-bpi-r3-mini.patch
-rm ./mt7986a.patch
+# cat ./mt7986a.patch > ./999-dts-mt7986a-bpi-r3-mini.patch
+cat ./mt7986a-bananapi-bpi-r3-mini.patch >> ./999-dts-mt7986a-bpi-r3-mini.patch
+# rm ./mt7986a.patch
 rm ./mt7986a-bananapi-bpi-r3-mini.patch
 
 # copy to patches directory
-cp ./196-dts-mt7986a-bpi-r3-mini.patch target/linux/mediatek/patches-5.15/196-dts-mt7986a-bpi-r3-mini.patch
-rm ./196-dts-mt7986a-bpi-r3-mini.patch
+cp ./999-dts-mt7986a-bpi-r3-mini.patch target/linux/mediatek/patches-5.15/999-dts-mt7986a-bpi-r3-mini.patch
+rm ./999-dts-mt7986a-bpi-r3-mini.patch
