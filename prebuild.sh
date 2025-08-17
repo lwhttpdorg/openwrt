ec5e11b56b4aa5fc14aad35891c185c3ecea8031
sandro <sandro@lwhttpd.com>
2025-08-18 02:21:16 +0800

    build: fix error

--- prebuild.sh
+++ prebuild.sh
@@ -19,7 +19,7 @@ rm -rf feeds/packages/utils/v2dat
 git apply ./feeds.diff
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 ./scripts/feeds install -a
 ./scripts/feeds install -a
